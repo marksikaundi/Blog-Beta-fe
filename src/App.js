46b166343b5edfd42fc7b6ea31b5dc11396c9915
marksikaundi <emmanuelmark117@gmail.com>
2023-07-20 21:26:49 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>Test</div>
+    <main>
+      <header>
+        <a href="" className="logo">
+          My blog
+        </a>
+        <nav>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+        <img
+          src="https://devcircleafrica.com/wp-content/uploads/2023/07/Meta-releases-latest-AI-model-for-commercial-use-1068x601.webp"
+          alt=""
+        />
+        </div>
+        
+        <div className="texts">
+          <h2>Meta is partnering with Microsoft to introduce Llama 2</h2>
+          <p>
+            This has helped us build better products by driving progress across
+            the industry. Open source drives innovation because it enables many
+            more developers to build with new technology.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img
+          src="https://devcircleafrica.com/wp-content/uploads/2023/07/Meta-releases-latest-AI-model-for-commercial-use-1068x601.webp"
+          alt=""
+        />
+        </div>
+        <div className="texts">
+          <h2>Meta is partnering with Microsoft to introduce Llama 2</h2>
+          <p>
+            This has helped us build better products by driving progress across
+            the industry. Open source drives innovation because it enables many
+            more developers to build with new technology.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img
+          src="https://devcircleafrica.com/wp-content/uploads/2023/07/Meta-releases-latest-AI-model-for-commercial-use-1068x601.webp"
+          alt=""
+        />
+        </div>
+        <div className="texts">
+          <h2>Meta is partnering with Microsoft to introduce Llama 2</h2>
+          <p>
+            This has helped us build better products by driving progress across
+            the industry. Open source drives innovation because it enables many
+            more developers to build with new technology.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
