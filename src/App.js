60d67dc0fcc195fd8d6a7fee4755ab12e2f10179
marksikaundi <emmanuelmark117@gmail.com>
2023-07-20 22:51:32 +0200

    added pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Layout from "./Layout";
 //import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={"/login"} element={<div>Login page</div>} />
-        <Route path={"/register"} element={<div>Register page</div>} />
+        <Route path={"/login"} element={<LoginPage/>} />
+        <Route path={"/register"} element={<RegisterPage/>} />
       </Route>
     </Routes>
   );
