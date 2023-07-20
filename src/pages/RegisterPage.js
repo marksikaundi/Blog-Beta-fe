fdc5c599a660da45ec85200f144436aaceb34790
marksikaundi <emmanuelmark117@gmail.com>
2023-07-21 01:05:52 +0200

    up

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -3,14 +3,36 @@ import { useState } from "react";
 export default function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  // async function register(ev) {
+  //   ev.preventDefault();
+  //   await fetch('http://localhost:4000', {
+  //     method: 'POST',
+  //     body: JSON.stringify({username, password}),
+  //     headers: {'Content-Type':'application/json'}
+  //   })
+  // }
   async function register(ev) {
     ev.preventDefault();
-    await fetch('http://localhost:4000/register', {
-      method: 'POST',
-      body: JSON.stringify({username, password}),
-      headers: {'Content-Type':'application/json'}
-    })
+    try {
+      const response = await fetch('http://localhost:4000/register', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // Registration successful, you can perform any additional actions here
+        console.log('Registration successful!');
+      } else {
+        // Handle error response here
+        console.error('Registration failed!');
+      }
+    } catch (error) {
+      // Handle network errors or other exceptions here
+      console.error('Error occurred during registration:', error);
+    }
   }
+  
 
   return (
     <div>
