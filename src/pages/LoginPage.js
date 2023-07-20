d7b96d71b4939c75d251a41e9a5c2acb1bfb4537
marksikaundi <emmanuelmark117@gmail.com>
2023-07-20 23:36:28 +0200

    styl

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,15 @@
 export default function LoginPage() {
   return (
     <div>
+      <div className="avatar-container">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/552/552721.png"
+          alt="Avatar"
+          className="avatar"
+        />
+      </div>
       <form className="login">
-      <h1>Login Account</h1>
+        <h1>Login Account</h1>
         <input type="text" placeholder="Enter Username" />
         <input type="password" placeholder="Enter Password" />
         <button>Login</button>
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,13 @@
 export default function RegisterPage() {
   return (
     <div>
+      <div className="avatar-container">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/552/552721.png"
+          alt="Avatar"
+          className="avatar"
+        />
+      </div>
       <form className="register">
         <h1>Register Account</h1>
         <input type="text" placeholder="Enter Username" />
