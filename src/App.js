329085894eccdd8586cd8dbf10d0455533745543
marksikaundi <emmanuelmark117@gmail.com>
2023-07-20 22:52:36 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={"/login"} element={<LoginPage/>} />
-        <Route path={"/register"} element={<RegisterPage/>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
