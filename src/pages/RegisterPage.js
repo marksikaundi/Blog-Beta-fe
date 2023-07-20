aefddfcd81373eedcf3f2920ef7ab1b2bf82bb77
marksikaundi <emmanuelmark117@gmail.com>
2023-07-21 00:43:31 +0200

    fr

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type':'application/json'}
+    })
+  }
+
   return (
     <div>
       <div className="avatar-container">
@@ -8,10 +21,20 @@ export default function RegisterPage() {
           className="avatar"
         />
       </div>
-      <form className="register">
+      <form className="register" onSubmit={register}>
         <h1>Register Account</h1>
-        <input type="text" placeholder="Enter Username" />
-        <input type="password" placeholder="Enter Password" />
+        <input
+          type="text"
+          placeholder="Enter Username"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
         <button>Register</button>
       </form>
     </div>
