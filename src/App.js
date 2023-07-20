8955671573852f89ed7301cfa23ba7a9b56d9c89
marksikaundi <emmanuelmark117@gmail.com>
2023-07-20 22:10:10 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
 import Post from "./components/Post";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <main>
-      <Header />
-      <Post />
-      <Post />
-      <Post />
-    </main>
+    <Routes>
+      <Route
+        index element={
+          <main>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
+          </main>
+        }
+      />
+    </Routes>
   );
 }
 
