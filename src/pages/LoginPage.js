21681bd587eb427fb096227d537e5f187fd9e969
marksikaundi <emmanuelmark117@gmail.com>
2023-07-20 23:04:47 +0200

    pr

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,11 @@
 export default function LoginPage() {
-    return(
-        <div>Welcome to login</div>
-    )
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder="Enter Username" />
+        <input type="password" placeholder="Enter Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,5 +1,10 @@
 export default function RegisterPage() {
     return(
-        <div>Welcome to register</div>
-    )
+<div>
+      <form action="">
+        <input type="text" placeholder="Enter Username" />
+        <input type="password" placeholder="Enter Password" />
+        <button>Register</button>
+      </form>
+    </div>    )
 }
