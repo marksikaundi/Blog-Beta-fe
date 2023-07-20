dd007d4a071f9f0fae512db9e65b53e42df9dd39
marksikaundi <emmanuelmark117@gmail.com>
2023-07-20 23:08:02 +0200

    pg

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,7 @@
 export default function LoginPage() {
   return (
     <div>
-      <form action="">
+      <form className="login">
         <input type="text" placeholder="Enter Username" />
         <input type="password" placeholder="Enter Password" />
         <button>Login</button>
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,11 @@
 export default function RegisterPage() {
-    return(
-<div>
-      <form action="">
+  return (
+    <div>
+      <form className="register">
         <input type="text" placeholder="Enter Username" />
         <input type="password" placeholder="Enter Password" />
         <button>Register</button>
       </form>
-    </div>    )
+    </div>
+  );
 }
