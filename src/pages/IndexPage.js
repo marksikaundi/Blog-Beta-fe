353ac6b44d014e61be33686401a010a6b117d283
marksikaundi <emmanuelmark117@gmail.com>
2023-07-26 22:52:13 +0200

    nothing much fe

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,3 +1,5 @@
+
+// mongodb+srv://blog:FOl5TbYqumQOEyyU@cluster0.gmbhnpb.mongodb.net/?retryWrites=true&w=majority
 import Post from "../components/Post"
 export default function IndexPage() {
     return(
